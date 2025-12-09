8238235aafb9f42182b746e9987a31f97c4806ba
maybedelusional <141055925+maybedelusional@users.noreply.github.com>
2025-12-09 21:14:32 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,6 @@
+return {
+    version = "1.2",
+    flex_mode = true,  -- set to false for normal, true for dev/flex
+    last_update_time = os.time() - (3*24*60*60), -- simulate last update was 3 days ago
+    needs_update = false
+}
